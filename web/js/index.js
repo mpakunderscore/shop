932c42d2d324275f6db5b01a9bd0101f8fb9b80d
Pavel Kuzmin <mpakunderscore@gmail.com>
2018-08-30 22:53:49 +0300

    Categories css fix

--- web/js/index.js
+++ web/js/index.js
@@ -1,26 +1,33 @@
 let state = {};
 state.items = [];
 
-let xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
+function getItems() {
 
-    if (this.readyState == 4 && this.status == 200) {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
 
-        state.items = JSON.parse(this.responseText);
+        if (this.readyState == 4 && this.status == 200) {
 
-        buildShop();
+            state.items = JSON.parse(this.responseText);
 
-        state.items['Корзина'] = {};
-        state.items['Корзина']['Пусто'] = [];
-        state.items['Корзина']['Пусто'].push({name: "Test name", images: "", description: "Some test description", price: 1900});
-        state.items['Корзина']['Пусто'].push({name: "Test name", images: "", description: "Some test description", price: 300});
-        state.items['Корзина']['Пусто'].push({name: "Test name", images: "", description: "Some test description", price: 400});
-    }
-};
+            buildShop();
 
-xhttp.open("GET", "/items/model", true);
-xhttp.setRequestHeader("Content-type", "application/json");
-xhttp.send("JSON");
+            state.items['Корзина'] = {};
+            state.items['Корзина']['Все'] = [];
+            // state.items['Корзина']['Пусто'].push({name: "Test name", images: "", description: "Some test description", price: 1900});
+            // state.items['Корзина']['Пусто'].push({name: "Test name", images: "", description: "Some test description", price: 300});
+            // state.items['Корзина']['Пусто'].push({name: "Test name", images: "", description: "Some test description", price: 400});
+        }
+    };
+
+    xhttp.open("GET", "/items/model", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send("JSON");
+}
+
+getItems();
+
+//Render
 
 function buildShop() {
 
@@ -107,6 +114,19 @@ function buildCartItems() {
     main.innerHTML += itemsHtml;
 }
 
+//HTML
+
+function buildBlockHtml(title) {
+
+    let blockHtml = "";
+
+    blockHtml += "<div class='block'>";
+    blockHtml += "<div class='title'>" + title + "</div>";
+    blockHtml += "</div>";
+
+    return blockHtml;
+}
+
 function buildItemsHtml(items, isCart) {
 
     let itemsHtml = "<div class='items'>";
@@ -118,20 +138,38 @@ function buildItemsHtml(items, isCart) {
             "<div class='images'><img src='" + items[i].images.split('\n')[0] + "'>" + "</div>" +
             "<div class='name'>" + items[i].name + "</div>" +
             "<div class='description'>" + items[i].description.replace(/(?:\r\n|\r|\n)/g, '<br/>') + "</div>" +
-            "<div class='price'>" + (items[i].price + "").replace("000", " 000") + " руб</div>";
+            "<div class='price'>" + (items[i].price + "") + " руб</div>";
 
         if (!isCart)
-            itemsHtml += "<div class='cart'><div class='cartBack'></div><span>В корзину</span><div class='electrocontact'></div></div>";
+            itemsHtml += "<div class='cart' onclick='addToCart(`" + items[i].name + "`)'><div class='cartBack'></div><span>В корзину</span></div>";
+
+    // <div class='electrocontact'></div>
 
         itemsHtml += "</div>";
     }
 
+    itemsHtml += "</div>";
+
     if (isCart)
         itemsHtml += "<div class='cart'><div class='cartBack'></div><span>Оплатить</span></div>";
 
-    itemsHtml += "</div>";
-
     return itemsHtml;
+}
+
+function addToCart(item) {
+
+    console.log(item)
+    state.items['Корзина']['Пусто'].push({name: item, images: "https://56.img.avito.st/640x480/4614810256.jpg", description: "Тип аккумулятора LTO\n" +
+        "Номинальное напряжение 2.3V\n" +
+        "Непрерывный ток разряда 10С (400A)\n" +
+        "Пиковый ток разряда 25С (1000A)\n" +
+        "Максимальный ток заряда 10С (400A)\n" +
+        "Рабочая температура разряда -50 ~ 65 ℃\n" +
+        "Рабочая температура заряда -30 ~ 65 ℃\n" +
+        "Внутреннее сопротивление <0,3mΩ\n" +
+        "\n" +
+        "Габариты 160x66x210 мм\n" +
+        "Вес 1100 г\n", price: 1900});
 }
 
 function buildCategoriesHtml(items, selectedCategory, isCart) {
@@ -147,24 +185,13 @@ function buildCategoriesHtml(items, selectedCategory, isCart) {
     return categoriesHtml;
 }
 
-function buildBlockHtml(title) {
-
-    let blockHtml = "";
-
-    blockHtml += "<div class='block'>";
-    blockHtml += "<div class='title'>" + title + "</div>";
-    blockHtml += "</div>";
-
-    return blockHtml;
-}
-
 function buildMainCategoriesHtml(title) {
 
     let array = Object.keys(state.items);
 
-    array = array.filter(function(item) {
-        return item !== title
-    });
+    // array = array.filter(function(item) {
+    //     return item !== title
+    // });
 
     array = array.filter(function(item) {
         return item !== "Корзина"
@@ -174,7 +201,7 @@ function buildMainCategoriesHtml(title) {
 
     buildMainCategoriesHtml += "<div class='main-categories'>";
     for (let i = 0; i < array.length; i++) {
-        buildMainCategoriesHtml += "<div class='' onclick='includeHTML(`shop/" + translate(array[i]) + "`)'>" + array[i] + "</div>";
+        buildMainCategoriesHtml += "<div class='" + (array[i] === title ? 'active' : '') + "' onclick='includeHTML(`shop/" + translate(array[i]) + "`)'>" + array[i] + "</div>";
     }
     buildMainCategoriesHtml += "</div>";
 
